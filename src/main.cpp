47cae35d644d3e27f53408d51621b0357e4f05ac
Necobi <274595030+Necobi@users.noreply.github.com>
2026-09-24 21:59:28 +0000

    Changes background color to blue and speed to 7.5

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,11 @@
 int main() {
     bn::core::init();
 
-    bn::backdrop::set_color(bn::color(15, 0, 0));
+    bn::backdrop::set_color(bn::color(0, 0, 30));
 
     auto dot = bn::sprite_items::bun.create_sprite(0, 0);
 
-    bn::fixed speed = 1.5;
+    bn::fixed speed = 7.5;
 
     bn::fixed dy = 0;
     bn::fixed gravity = .03;
